19d7a274d499c57d12d277138b5ac9be94371989
karlorz <karl.logon@gmail.com>
2024-04-21 20:48:21 +0800

    update

--- tests/Unit/RoomTest.php
+++ tests/Unit/RoomTest.php
@@ -9,13 +9,26 @@ use App\Room;
 class RoomTest extends TestCase
 {
     /**
-     * A basic unit test example.
+     * Test the has() method in Room class
+     *
+     * @return void
      */
     public function test_room_has(): void
     {
         $room = new Room(["Jack", "Peter", "Amy"]); // Create a new room
         $this->assertTrue($room->has("Jack")); // Expect true
         $this->assertFalse($room->has("Eric")); // Expect false
+    }
+
+    /**
+     * Test the add() method in Room class
+     *
+     * @return void
+     */
+    public function test_room_add()
+    {
+        $room = new Room(["Jack"]); // Create a new room
+        $this->assertContains("Peter", $room->add("Peter"));
     }
     
 }
