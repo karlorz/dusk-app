ed64011c9551f859788003495a3cbe746435f8c7
karlorz <karl.logon@gmail.com>
2024-04-24 23:03:17 +0800

    update

--- routes/web.php
+++ routes/web.php
@@ -27,3 +27,15 @@ Route::get('/json-test', function () {
 Route::get('/view-test', function () {
     return view('test', ['name' => 'Taylor']);
 });
+
+Route::group(['middleware' => 'guest'], function () {
+    Route::get('/register', [AuthController::class, 'register'])->name('register');
+    Route::post('/register', [AuthController::class, 'registerPost'])->name('register');
+    Route::get('/login', [AuthController::class, 'login'])->name('login');
+    Route::post('/login', [AuthController::class, 'loginPost'])->name('login');
+});
+ 
+Route::group(['middleware' => 'auth'], function () {
+    Route::get('/home', [HomeController::class, 'index']);
+    Route::delete('/logout', [AuthController::class, 'logout'])->name('logout');
+});
